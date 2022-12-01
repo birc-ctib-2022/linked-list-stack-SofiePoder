6cfaeca5aec7323eba9d19dca467f93092bf4f5e
SofiePoder <201905630@post.au.dk>
2022-12-01 16:44:24 +0100

    pop() not working yet

--- src/stack.py
+++ src/stack.py
@@ -23,20 +23,49 @@ class Stack(Generic[T]):
 
     def __init__(self) -> None:
         """Create a new stack of values of type T."""
-        # FIXME: code here
+        self.stack = None
 
-    def push(self, x: T) -> None:
+    def push(self, x: T) -> None: 
         """Push x on the top of this stack."""
-        # FIXME: code here
+        self.stack = Link(x,self.stack)
+        return self.stack
 
     def top(self) -> T:
         """Return the top of the stack."""
-        # FIXME: code here
+        return self.stack.head
 
-    def pop(self) -> T:
+    def pop(self) -> T: #not working
         """Pop the top element off the stack and return it."""
-        # FIXME: code here
+        
+        if self.stack.head == None:
+            return None
+        else:
+            self.stack.head = self.stack.tail
+        return self.stack.head
+        
 
     def is_empty(self) -> bool:
         """Test if the stack is empty."""
-        # FIXME: code here
+        if self.stack == None:
+            return "stack is empty"
+        else:
+            return "stack is not empty"
+
+
+
+my_stack = Stack()
+my_stack.push(1)
+print(my_stack.push(2))
+
+print(my_stack.top())
+
+print(my_stack.pop())
+
+print(my_stack.pop())
+
+new_stack = Stack()
+print(new_stack.is_empty())
+
+
+
+
